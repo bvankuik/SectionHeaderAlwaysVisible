8268d67e729730cc3cd107ff4ba5907f241a9ec0
Bart van Kuik <bart@dutchvirtual.nl>
2016-04-11 10:14:42 +0200

    Empty Xcode project

---
+++ SectionHeaderAlwaysVisible/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SectionHeaderAlwaysVisible
+//
+//  Created by Bart van Kuik on 11/04/16.
+//  Copyright © 2016 DutchVirtual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
